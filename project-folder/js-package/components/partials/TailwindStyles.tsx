bbf2f003e8eea587a469d85f8f9c9d8f963b6386
Chris Stone <chris.stone603@gmail.com>
2024-03-13 16:46:24 -0400

    feat: creates a component with tailwind styles; includes this in post template; styles post page

---
+++ project-folder/js-package/components/partials/TailwindStyles.tsx
@@ -0,0 +1,10 @@
+import '../../styles/fonts.css';
+import '../../styles/tailwind.css';
+
+const TailwindStyles = () => {
+  // This component doesn't need to render anything itself,
+  // as it's just used to ensure global styles are included.
+  return null;
+};
+
+export default TailwindStyles;
