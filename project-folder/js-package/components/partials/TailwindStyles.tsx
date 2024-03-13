032a80895cf1f31853c787d2ee711fe0ac52d76b
Chris Stone <chris.stone603@gmail.com>
2024-03-13 18:51:52 -0400

    feat: removes unused tailwind import

--- project-folder/js-package/components/partials/TailwindStyles.tsx
+++ project-folder/js-package/components/partials/TailwindStyles.tsx
@@ -2,8 +2,8 @@ import '../../styles/fonts.css';
 import '../../styles/tailwind.css';
 
 const TailwindStyles = () => {
-  // This component doesn't need to render anything itself,
-  // as it's just used to ensure global styles are included.
+  // This component render anything itself, as it's just
+  // used to ensure global styles are included.
   return null;
 };
 
