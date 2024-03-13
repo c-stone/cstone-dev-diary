bca5a87c07a9c0b122085b836d6b1b98e7839ce8
Chris Stone <chris.stone603@gmail.com>
2024-03-13 13:17:17 -0400

    creates blog content module

---
+++ project-folder/js-package/components/modules/BlogContent/index.tsx
@@ -0,0 +1,8 @@
+export const Component = () => {};
+
+export { fields } from './fields.tsx';
+export const meta = {
+  label: `Blog Post Content`,
+  host_template_types: ['BLOG_POST'],
+  categories: ['blog'],
+};
