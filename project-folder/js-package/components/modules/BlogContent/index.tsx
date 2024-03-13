c64802e6b27dd2afd951ad5e30e5a3139884a45c
Chris Stone <chris.stone603@gmail.com>
2024-03-13 13:33:59 -0400

    feat: adds required post content to post page

--- project-folder/js-package/components/modules/BlogContent/index.tsx
+++
@@ -1,8 +0,0 @@
-export const Component = () => {};
-
-export { fields } from './fields.tsx';
-export const meta = {
-  label: `Blog Post Content`,
-  host_template_types: ['BLOG_POST'],
-  categories: ['blog'],
-};
